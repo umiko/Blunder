82ca5f8ab4c981fdb76e9466eb10e668a1ddd49d
umiko <robertscholl.private@gmail.com>
2019-01-31 14:36:09 +0100

    Added Resource manager and load the objects object directly

--- Resource/Script/Blunder.js
+++ Resource/Script/Blunder.js
@@ -9,7 +9,11 @@ function main(){
 class Blunder{
 
     async loadResources() {
-        let manifest = Utility.loadJSONResource("./Resource/manifest.json");
+        let manifest = await Utility.loadJSONResource("./Resource/manifest.json")
+            .then(result => result.objects);
+        console.log(manifest);
+        //durch properties gehen, ist kein array
+        manifest.map(object.proper => RenderResourceManager.getInstance().loadObjectResources(object))
     }
 
     initializeResources() {
@@ -28,5 +32,66 @@ class Utility{
 
     static loadJSONResource(url){
         return this.loadTextResourceFromFile(url).then(file => JSON.parse(file));
+    }
+}
+
+class RenderResourceManager{
+
+    constructor(){
+        this.textures = [];
+        this.shaders = [];
+        this.meshes = [];
+    }
+
+    loadObjectResources(object){
+        console.log(object);
+    }
+
+    insertTexture(texture){
+        if(this.textures.includes(texture)){
+            return this.textures.indexOf(texture);
+        }
+        else{
+            return this.textures.push(texture)-1;
+        }
+    }
+
+    getTexture(textureIndex){
+        return this.textures[textureIndex];
+    }
+
+    insertShader(shader){
+        if(this.shaders.includes(shader)){
+            return this.shaders.indexOf(shader);
+        }
+        else{
+            return this.shaders.push(shader)-1;
+        }
+    }
+
+    getShader(shaderIndex){
+        return this.shaders[shaderIndex];
+    }
+
+    insertMesh(mesh){
+        if(this.meshes.includes(mesh)){
+            return this.meshes.indexOf(mesh);
+        }
+        else{
+            return this.meshes.push(mesh)-1;
+        }
+    }
+
+    getMesh(meshIndex){
+        return this.meshes[meshIndex];
+    }
+
+    static getInstance() {
+        if(this.instance){
+            return this.instance;
+        }
+        else{
+            this.instance = new RenderResourceManager();
+        }
     }
 }
